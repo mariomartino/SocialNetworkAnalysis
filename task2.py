51e4a485085c8d5c7c1c28c8f3be01fa40a99d41
Stefano Menduti <s.menduti1@studenti.unisa.it>
2022-05-11 16:15:13 +0200

    Degree, Closeness, Betweenness

--- task2.py
+++ task2.py
@@ -57,31 +57,66 @@ def closeness(G):
                     queue.append(w)
                     visited.add(w)
                     dist[w]=dist[v]+1
-
         cen[u]=sum(dist.values())
 
     return cen
 
+def betweenness(G):
+    edge_btw={frozenset(e):0 for e in G.edges()}
+    node_btw={i:0 for i in G.nodes()}
+
+    for s in G.nodes():
+        tree=[] 
+        spnum={i:0 for i in G.nodes()} 
+        parents={i:[] for i in G.nodes()} 
+        distance={i:-1 for i in G.nodes()} 
+        eflow={frozenset(e):0 for e in G.edges()} 
+        vflow={i:1 for i in G.nodes()} 
+
+        queue=[s]
+        spnum[s]=1
+        distance[s]=0
+        while queue != []:
+            c=queue.pop(0)
+            tree.append(c)
+            for i in G[c]:
+                if distance[i] == -1: 
+                    queue.append(i)
+                    distance[i]=distance[c]+1
+                if distance[i] == distance[c]+1: 
+                    spnum[i]+=spnum[c]
+                    parents[i].append(c)
+
+        while tree != []:
+            c=tree.pop()
+            for i in parents[c]:
+                eflow[frozenset({c,i})]+=vflow[c] * (spnum[i]/spnum[c])
+                vflow[i]+=eflow[frozenset({c,i})] 
+                edge_btw[frozenset({c,i})]+=eflow[frozenset({c,i})] 
+            if c != s:
+                node_btw[c]+=vflow[c] 
+
+    return edge_btw,node_btw
+
+def btw(G):
+    return betweenness(G)[1]
+
+
+
+
+###########################################################################
+## Main test ##
 G = utils.load_node("email-Eu-core.txt", True, " ")
 
-page_rank(G)
-results = list(G.nodes(data="weight"))
-results.sort(key=lambda tup:tup[1], reverse=True)
-
-#Stampa del page rank in ordine decrescente
-#for u in results:
-#  print(u)
-
-print(results[0])
-
 cen = degree(G)
-clo = degree(G)
+clo = closeness(G)
+bet = btw(G)
+page_rank(G)
 
 cen_sort = list(cen.items())
 cen_sort.sort(key= lambda tup:tup[1], reverse=True)
-# Essendo clo e cen uguali, basta stamparne uno
 
-# Stampa di tutti i nodi con la loro degree/closeness
+# Stampa di tutti i nodi con la loro degree
 #for key, value in cen.items():
 #  print(str(key) + ": " + str(value))
 
@@ -89,4 +124,36 @@ cen_sort.sort(key= lambda tup:tup[1], reverse=True)
 #for k in cen_sort:
 #  print(k)
 
-print(cen_sort[0])
+clo_sort = list(clo.items())
+clo_sort.sort(key= lambda tup:tup[1], reverse=True)
+
+# Stampa di tutti i nodi con la loro closeness
+#for key, value in clo.items():
+#  print(str(key) + ": " + str(value))
+
+#Stampa dei nodi ordinati decrescente
+#for k in clo_sort:
+#  print(k)
+
+bet_sort = list(bet.items())
+bet_sort.sort(key= lambda tup:tup[1], reverse=True)
+
+# Stampa di tutti i nodi con la loro betweeness
+#for key, value in bet.items():
+#  print(str(key) + ": " + str(value))
+
+#Stampa dei nodi ordinati decrescente
+#for k in bet_sort:
+#  print(k)
+
+results = list(G.nodes(data="weight"))
+results.sort(key=lambda tup:tup[1], reverse=True)
+
+#Stampa del page rank in ordine decrescente
+#for u in results:
+#  print(u)
+
+print("Degree: "+str(cen_sort[0]))
+print("Closeness: "+str(clo_sort[0]))
+print("Betweenness: "+str(bet_sort[0]))
+print("Page Rank: "+str(results[0]))
