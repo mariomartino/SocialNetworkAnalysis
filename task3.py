c43d83973fb693af6e435f1302f8e38e3ba5e54b
Stefano Menduti <s.menduti1@studenti.unisa.it>
2022-05-30 13:12:35 +0200

    More details on Epsilon-Greedy

--- task3.py
+++ task3.py
@@ -4,7 +4,7 @@ import networkx as nx
 import utils
 
 def epsilon(arms,rounds,e):
-  reward = 0
+  reward = dict({a :0 for a in arms.keys()})
   for t in rounds:
     r = random.random()
     if r > e:
@@ -12,10 +12,11 @@ def epsilon(arms,rounds,e):
     else:
       arm = list(arms.values()).index(max(arms.values()))
     
-    reward += arms[arm]
+    reward[arm] = reward[arm] + arms[arm]
 
-
-
+  for k,v in reward.items():
+    if v > 0:
+      print(str(k) + ": " + str(v))
 
 ###############################################
 ## Main test ##
@@ -24,9 +25,10 @@ G = utils.load_node("email-Eu-core.txt", True, " ")
 
 arms = dict()
 for i in range(0,99):
-  arms[i] = random.randint(1,100)
+  #arms[i] = random.randint(1,100)
+  arms[i] = 1
 
-rounds = np.arange(20)
-e = 0.4
+rounds = np.arange(100)
+e = 0.3
 
 epsilon(arms,rounds,e)
