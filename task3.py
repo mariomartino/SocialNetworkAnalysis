4366b047015511f82ddf856f4364a9f23b64b3ee
SteMeno <75124338+SteMeno@users.noreply.github.com>
2022-05-31 13:50:51 +0200

    Implementation environment for epsilon-greedy

--- task3.py
+++ task3.py
@@ -1,34 +1,57 @@
+import copy
 import random
 import numpy as np
 import networkx as nx
 import utils
 
 def epsilon(arms,rounds,e):
+  copy_arm = copy.deepcopy(arms)
   reward = dict({a :0 for a in arms.keys()})
   for t in rounds:
     r = random.random()
     if r > e:
-      arm = random.choice(list(arms.keys()))
+      arm = random.choice(list(copy_arm.keys()))
     else:
-      arm = list(arms.values()).index(max(arms.values()))
+      arm = list(copy_arm.values()).index(max(copy_arm.values()))
     
-    reward[arm] = reward[arm] + arms[arm]
+    reward[arm] = reward[arm] + copy_arm[arm]
+    copy_arm[arm] = 0
 
-  for k,v in reward.items():
-    if v > 0:
-      print(str(k) + ": " + str(v))
+  best_arm = list(reward.items())
+  best_arm.sort(key=lambda tup:tup[1], reverse=True)
+  return best_arm[0]
+  
 
 ###############################################
 ## Main test ##
 
 G = utils.load_node("email-Eu-core.txt", True, " ")
 
-arms = dict()
-for i in range(0,99):
-  #arms[i] = random.randint(1,100)
-  arms[i] = 1
+aucts = dict()
+for i in range(0,40):
+  aucts[i] = dict()
+  for a in range(0,100):
+    aucts[i][a] = random.random()
 
-rounds = np.arange(100)
-e = 0.3
+advs = dict()
+for c in range(0,10):
+  advs[c] = random.random()
 
-epsilon(arms,rounds,e)
+rounds = np.arange(0,100,1)
+
+res = dict()
+
+temp = copy.deepcopy(aucts)
+for c in range(len(advs.keys())):
+  partial = dict()
+  for a in temp.keys():
+    partial[a] = epsilon(temp[a], rounds, advs[c])
+  
+  best_auct = list(partial.items())
+  best_auct.sort(key=lambda tup:tup[1], reverse=True)
+  res[c] = best_auct[0][0]
+
+  del temp[best_auct[0][0]]
+
+for k,v in res.items():
+  print("ADV "+str(k)+": Auctioner "+str(v))
