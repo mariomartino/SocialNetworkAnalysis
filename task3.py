df511027035f0356d1830e066232e15b10cf2208
SteMeno <75124338+SteMeno@users.noreply.github.com>
2022-06-04 12:37:48 +0200

    Complete task3 implementation

--- task3.py
+++ task3.py
@@ -4,57 +4,60 @@ import numpy as np
 import networkx as nx
 import utils
 
-def epsilon(arms,rounds,e):
-  copy_arm = copy.deepcopy(arms)
-  reward = dict({a :0 for a in arms.keys()})
-  for t in rounds:
-    r = random.random()
-    if r > e:
-      arm = random.choice(list(copy_arm.keys()))
-    else:
-      arm = list(copy_arm.values()).index(max(copy_arm.values()))
-    
-    reward[arm] = reward[arm] + copy_arm[arm]
-    copy_arm[arm] = 0
-
-  best_arm = list(reward.items())
-  best_arm.sort(key=lambda tup:tup[1], reverse=True)
-  return best_arm[0]
+def epsilon(auctioners, advertisers, e):
+  r1 = random.random()
+  if r1 < e:
+    arm = random.choice(list(advertisers.keys()))  
+  else:
+    arm = max(auctioners, key = lambda k: auctioners[k])
   
+  return arm 
 
-###############################################
-## Main test ##
+########################################################################################
 
-G = utils.load_node("email-Eu-core.txt", True, " ")
-
-aucts = dict()
-for i in range(0,40):
-  aucts[i] = dict()
-  for a in range(0,100):
-    aucts[i][a] = random.random()
-
-advs = dict()
-payment = 10000
+slot= dict()
 for c in range(0,10):
-  advs[c] = {'payment': payment, 'ctr': random.random()}
-  payment -= 1000
+  slot[c] = dict()
+  for i in range(0,40):
+    slot[c][i] = random.random()
 
-rounds = np.arange(0,100,1)
+advertisers = dict()
+for i in range(0,40):
+  advertisers[i] = dict()
+  for a in range(0,len(slot.keys())):
+    advertisers[i][a] = random.random()
 
-res = dict()
+auctioners = dict()
+chosen = dict()
+for c in range(0,len(slot.keys())):
+  auctioners[c] = dict()
+  chosen[c] = dict()
+  for i in range(0,len(advertisers.keys())):
+    auctioners[c][i] = 0
+    chosen[c][i] = 0
 
-temp = copy.deepcopy(aucts)
-for c in range(len(advs.keys())):
-  partial = dict()
-  for a in temp.keys():
-    partial[a] = epsilon(temp[a], rounds, advs[c]['ctr'])
+rounds = np.arange(0,100000,1)
+for c in range(len(slot.keys())):
+  for a in rounds:
+    value = epsilon(auctioners[c], advertisers, 0.01)
+    chosen[c][value] += 1
+    r = random.random()
+    if r < slot[c][value]:
+      if chosen[c][value] > 1:
+        auctioners[c][value] += (advertisers[value][c]/(chosen[c][value] - 1))
+      else:
+        auctioners[c][value] += advertisers[value][c]
+
+
+    if chosen[c][value] > 1:
+      auctioners[c][value] *= ((chosen[c][value] - 1)/chosen[c][value]) 
   
-  best_auct = list(partial.items())
-  best_auct.sort(key=lambda tup:tup[1], reverse=True)
-  res[c] = {'auct': best_auct[0][0],'pay': advs[c]['payment']}
+for k,v in auctioners.items():
+  print(str(k))
+  for i in v.values():
+    print("     "+str(i))
 
-  del temp[best_auct[0][0]]
-
-for k,v in res.items():
-  print("ADV "+str(k)+": Auctioner "+str(v['auct'])+", Pay: "+str(v['pay']))
-  
+##### da fare ##########################
+# Creazione Ambiente
+# Eventualmente, programmazione a classi
+# Decidere l'epsilon
