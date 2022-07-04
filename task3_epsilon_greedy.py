72bdbaf33edd2fe135d26dc93540e61c95a46bec
mariomartino <m.martino41@studenti.unisa.it>
2022-07-04 16:28:43 +0200

    Refactor the DocString for EpsilonGreedy computation

--- task3_epsilon_greedy.py
+++ task3_epsilon_greedy.py
@@ -4,8 +4,9 @@ def epsilon_greedy(choices, expected_values, e):
   """A method launching an Epsilon-Greedy experiment. The function with probability e returns a random choice, otherwile returns
   the best one. 
   Args:
-      choices (list)): A list containing the possible choices of the algorithm
-      expected_values (dict): A dictionary contaning the expected values related to the possible choice. Format: ("choice" : "expected value")
+      choices (list): A list containing the possible choices of the algorithm
+      expected_values (dict): A dictionary contaning the expected values related to the possible choice. 
+                              Format: ("choice" : "expected value")
       e (float): The epsilon value of the algorithm
   Returns:
       int: The integer representing the choice of the round
